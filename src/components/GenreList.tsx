a17be315daea785fe9612ff4b4f8ec5060f3a23d
Tiffany Fu <tiffany.fu7@gmail.com>
2023-08-01 12:21:23 -0400

    Filter Games by Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
-import useData from '../hooks/useData';
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({onSelectGenre}: Props) => {
     const { data, isLoading, error } = useGenres();
 
     if (error) return null;
@@ -19,7 +22,7 @@ const GenreList = () => {
                     borderRadius={8}
                     src={getCroppedImageUrl(genre.image_background)}
                 />
-                <Text fontSize='lg'>{genre.name}</Text>    
+              <Button onClick={() => onSelectGenre(genre)}  fontSize='lg' variant='link'>{genre.name}</Button>    
             </HStack>
           </ListItem>
         ))}
@@ -27,4 +30,4 @@ const GenreList = () => {
     );
 }
 
-export default GenreList
+export default GenreList;
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -26,7 +26,12 @@ const PlatformIconList = ({ platforms }: Props) => {
 
     return (
     <HStack marginY={1}>
-      {platforms.map(platform => <Icon as={iconMap[platform.slug]} color='gray.400'/>)}
+        {platforms.map(platform =>
+          <Icon
+            key={platform.id}
+            as={iconMap[platform.slug]}
+            color='gray.400'
+          />)}
     </HStack>
   );
 }
